7747ed183724d0978565c8bfe58bbd38dd046520
knightza123 <knightza123@hotmail.com>
2018-02-05 17:15:30 +0700

    finish book

--- Book.java
+++ Book.java
@@ -3,9 +3,23 @@ public class Book{
 	private int pagenumber;
 	private String Author;
 	public Book(String title,int pagenumber,String Author){
+		//รับ title pangenumber author
 		this.title  = title;
 		this.pagenumber = pagenumber;
 		this.Author = Author;
+	}
+	public Book(String title, int pagenumber){
+		//รับ  title pagenumber
+		this.title  = title;
+		this.pagenumber = pagenumber;
+	}
+	public Book(String title){
+		//รับแค่ title
+		this.title = title;
+	}
+	public Book(){
+		//ไม่รับอะไรเลย
+
 	}
 	public String getTitle(){
 		return title;
@@ -26,6 +40,14 @@ public class Book{
 		this.Author = Author;
 	}
 	public void showbook(){
-		System.out.printn(title+':'+pagenumber+':'+Author);
+		System.out.println(title+':'+pagenumber+':'+Author);
+	}
+	public boolean sameBook(String title, int pagenumber, String Author){
+		if (this.title == title && this.pagenumber == pagenumber && this.Author == Author) {
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 }
--- Mybook.java
+++ Mybook.java
@@ -1,5 +1,29 @@
 public class Mybook{
 	public static void main(String[] args){
+		Book   mybook1; 
+		mybook1 = new Book(); 
+		Book mybook2;
+		mybook2 = new Book("Java Programming");
+		Book mybook3;
+		mybook3 = new Book("C Programming",500);
+		
 
+		mybook1.setTitle("OOP");
+		mybook1.setPagenumber(1000);
+		mybook2.setTitle("Netwrok");
+		mybook2.setPagenumber(600);
+	
+
+		mybook1.showbook();
+		mybook2.showbook();
+		mybook3.showbook();
+		mybook1.setAuthor("JK");
+		mybook1.showbook();
+		if(mybook1.sameBook("oop",100,"JK")){
+			 System.out.println("true");
+
+		}else{
+			 System.out.println("flase");
+		}
 	}
 }
