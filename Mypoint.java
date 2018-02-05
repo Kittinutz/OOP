26718c09da5089f2cdd35ec4f1552ff9f34c66a0
knightza123 <knightza123@hotmail.com>
2018-02-05 18:39:19 +0700

    fix

--- Mypoint.java
+++ Mypoint.java
@@ -2,12 +2,14 @@ public class Mypoint{
 	public static void main(String[] args){
 		Point pt1 =  new Point(2,3,4);
 		Point pt2 = new Point(1,3,5);
-		p1.showPoint();
-		p2.showPoint();
-		p2.Movewithvalue(5,6,7);
-		p2.showPoint();
 
-		p1.MovewithPoint(p2);
-		p1.showPoint();
+		pt1.showPoint();
+		pt2.showPoint();
+		pt2.Movewithvalue(5,6,7);
+		pt2.showPoint();
+
+		pt1.MovewithPoint(pt2);
+		pt1.showPoint();
 	}
+
 }
