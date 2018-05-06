da0c8bf0d5699576239dc27745b0af16edb476ee
Kittinut Pramhan <knightza94@gmail.com>
2018-05-06 22:12:10 +0700

    exception2

--- lab9/Exception2.java
+++ lab9/Exception2.java
@@ -1,17 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
 import java.io.File;
+import java.io.*;
 public class Exception2{
 	public static void main(String[] args){
 		int total = 0;
 		int num = 0;
 		File myFile = null;
 		Scanner inputFile = null;
+		try{
 			myFile = new File("inFile.txt");
 			inputFile = new Scanner(myFile);
 			while(inputFile.hasNext()){
 					num = inputFile.nextInt();
 					total += num;
 			}
+		}catch(FileNotFoundException fnf){
+			System.out.println(fnf.getMessage());
+		}
+			
+			
 		System.out.println("The total value is " + total);
 	}
 }
